05d4de03101e37ecc6454406dbb735fcfa038557
RioAp13 <103746325+RioAp13@users.noreply.github.com>
2022-04-15 18:13:21 +0700

    Add files via upload

---
+++ app/Views/home.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Creative CV</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?= base_url()?>/assets/css/aos.css?ver=1.1.0" rel="stylesheet">
+    <link href="<?= base_url()?>/assets/css/bootstrap.min.css?ver=1.1.0" rel="stylesheet">
+    <link href="<?= base_url()?>/assets/css/main.css?ver=1.1.0" rel="stylesheet">
+    <noscript>
+      <style type="text/css">
+        [data-aos] {
+            opacity: 1 !important;
+            transform: translate(0) scale(1) !important;
+        }
+      </style>
+    </noscript>
+  </head>
+  <body id="top">
+    <header>
+      <div class="profile-page sidebar-collapse">
+        <nav class="navbar navbar-expand-lg fixed-top navbar-transparent bg-primary" color-on-scroll="400">
+          <div class="container">
+            <div class="navbar-translate"><a class="navbar-brand" href="#" rel="tooltip">Creative CV</a>
+              <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-bar bar1"></span><span class="navbar-toggler-bar bar2"></span><span class="navbar-toggler-bar bar3"></span></button>
+            </div>
+            <div class="collapse navbar-collapse justify-content-end" id="navigation">
+              <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link smooth-scroll" href="#about">Tentang Saya</a></li>
+                <li class="nav-item"><a class="nav-link smooth-scroll" href="#skill">
+                keterampilan</a></li>
+                <li class="nav-item"><a class="nav-link smooth-scroll" href="#experience">Pengalaman</a></li>
+                <li class="nav-item"><a class="nav-link smooth-scroll" href="#contact">
+                Kontak</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <div class="page-content">
+      <div>
+<div class="profile-page">
+  <div class="wrapper">
+    <div class="page-header page-header-small" filter-color="green">
+      <div class="page-header-image" data-parallax="true" style="background-image: url('<?=base_url()?>/assets/images/BC.jpg')"></div>
+      <div class="container">
+        <div class="content-center">
+          <div class="cc-profile-image"><a href="#"><img src="<?=base_url()?>/assets/images/RIO.jpg" alt="Image"/></a></div>
+          <div class="h2 title">Rio Agung Prayoga</div>
+          <p class="category text-white">Web Developer, Graphic Designer,  Editor</p>
+        </div>
+      </div>
+      <div class="section">
+        <div class="container">
+          <div class="button-container">
+              <a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on Facebook"><i class="fa fa-facebook"></i></a>
+              <a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on gmail"><i class="fa fa-envelope"></i></a>
+              <a class="btn btn-default btn-round btn-lg btn-icon" href="#" rel="tooltip" title="Follow me on Instagram"><i class="fa fa-instagram"></i></a>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="section" id="about">
+  <div class="container">
+    <div class="card" data-aos="fade-up" data-aos-offset="10">
+      <div class="row">
+        <div class="col-lg-6 col-md-12">
+          <div class="card-body">
+            <div class="h4 mt-0 title">Tentang Saya </div>
+            <p>Hallo Saya Rio Agung Prayoga, Saya adalah mahasiswa teknik informatika yang sedang mencari pekerjaan dan pengalaman hidup yang saya sukai.</p>
+            <p>Saya juga seorang yang suka sebuah tangtangan dan menyukai jalan-jalan ke berbagai daerah.<a href="https://templateflip.com/templates/creative-cv/" target="_blank">Learn More</a></p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-12">
+          <div class="card-body">
+            <div class="h4 mt-0 title">Informasi Dasar</div>
+            <div class="row">
+              <div class="col-sm-4"><strong class="text-uppercase">Umur:</strong></div>
+              <div class="col-sm-8">21</div>
+            </div>
+            <div class="row mt-3">
+              <div class="col-sm-4"><strong class="text-uppercase">Email:</strong></div>
+              <div class="col-sm-8">Rio,agungparayoga@gmail.com</div>
+            </div>
+            <div class="row mt-3">
+              <div class="col-sm-4"><strong class="text-uppercase">Telepon:</strong></div>
+              <div class="col-sm-8">+62 895-3651-04706</div>
+            </div>
+            <div class="row mt-3">
+              <div class="col-sm-4"><strong class="text-uppercase">Alamat:</strong></div>
+              <div class="col-sm-8">Cianjur, Jawa Barat, Indonesia</div>
+            </div>
+            <div class="row mt-3">
+              <div class="col-sm-4"><strong class="text-uppercase">Bahasa:</strong></div>
+              <div class="col-sm-8">Indonesia, Sunda, English</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="section" id="skill">
+  <div class="container">
+    <div class="h4 text-center mb-4 title">Keterampilan Profesional</div>
+    <div class="card" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">Microsoft Word</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div><span class="progress-value">80%</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">Microsoft Excel</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 75%;"></div><span class="progress-value">75%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">PHOTOSHOP</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div><span class="progress-value">65%</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">HTML</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div><span class="progress-value">55%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">JAVASCRIPT</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 75%;"></div><span class="progress-value">45%</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="progress-container progress-primary"><span class="progress-badge">PHP</span>
+              <div class="progress">
+                <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 70%;"></div><span class="progress-value">55%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section" id="experience">
+  <div class="container cc-experience">
+    <div class="h4 text-center mb-4 title">Pengalaman Kerja</div>
+    <div class="card">
+      <div class="row">
+        <div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body cc-experience-header">
+            <p>April 2017 - Juni 2017</p>
+            <div class="h5">PKL </div>Radio Tjandra FM CIANJUR
+          </div>
+        </div>
+        <div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body">
+            <div class="h5">EDITOR MUSIK & STAFF ADMIN</div>
+            <p>Pengalaman pertama saya Praktek kerja Lapangan di Radio Tjandra FM Cianjur di sana saya menjadi editor music,
+                aplikasi yang saya pakai untuk mengedit musik yaitu Adobe Audition, kemudian saya juga pernah menjadi Staff Admin,
+                yang bertugas memegang semua akun sosial media tjandra fm Cianjur. 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="row">
+        <div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body cc-experience-header">
+            <p>September 2020 - Januari 2021</p>
+            <div class="h5">Magang</div>
+          </div>
+        </div>
+        <div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body">
+            <div class="h5">EDITOR PHOTOSHOP</div>
+            <P>Pengalaman kedua saya yaitu pernah menjadi editor di sebuah perusahaan, dimana saya disana di tugas kan mengedit foto & membuat gambar untuk majalah anak-anak. </P>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="section">
+  <div class="container cc-education">
+    <div class="h4 text-center mb-4 title">Pendidikan</div>
+    <div class="card">
+      <div class="row">
+        <div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body cc-education-header">
+            <p>2019 - 2023</p>
+            <div class="h5">S.Kom</div>
+          </div>
+        </div>
+        <div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+          <div class="card-body">
+            <div class="h5">TEKNIK INFORMATIKA</div>
+            <p class="category">Universitas Muhammadiyah Sukabumi</p>
+            <p>Aktif di Organisasi Himpunan Mahasiswa & mengikuti Unit Kegiatan Mahasiswa (Mapalu)</p>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="" id="contact">
+  <div class="cc-contact-information">
+    <div class="container">
+      <div class="cc-contact pt-5">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="h4 text-center title pb-1" data-aos="zoom-in">KONTAK</div>
+            <div class="card mb-0" data-aos="zoom-in">
+              <div class="h4 text-center title">Hubungi Saya</div>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="card-body">
+                    <form action="https://formspree.io/your@email.com" method="POST">
+                      <div class="p pb-3"><strong></strong></div>
+                      <div class="row mb-3">
+                        <div class="col">
+                          <div class="input-group"><span class="input-group-addon"><i class="fa fa-user-circle"></i></span>
+                            <input class="form-control" type="text" name="name" placeholder="nama" required="required"/>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row mb-3">
+                        <div class="col">
+                          <div class="input-group"><span class="input-group-addon"><i class="fa fa-file-text"></i></span>
+                            <input class="form-control" type="text" name="Subject" placeholder="Subjek" required="required"/>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row mb-3">
+                        <div class="col">
+                          <div class="input-group"><span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                            <input class="form-control" type="email" name="_replyto" placeholder="Email" required="required"/>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row mb-3">
+                        <div class="col">
+                          <div class="form-group">
+                            <textarea class="form-control" name="message" placeholder="Pesan" required="required"></textarea>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col">
+                          <button class="btn btn-primary" type="submit">Kirim</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="card-body">
+                    <p class="mb-0"><strong>Alamat </strong></p>
+                    <p class="pb-2">Cianjur, Jawa Barat, Indonesia</p>
+                    <p class="mb-0"><strong>Telepon</strong></p>
+                    <p class="pb-2">+62 895-3651-04706</p>
+                    <p class="mb-0"><strong>Email</strong></p>
+                    <p>Rio.agungprayoga@gmail.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <footer class="footer">
+      <div class="container text-center">
+          <a class="cc-facebook btn btn-link" href="#"><i class="fa fa-facebook fa-2x " aria-hidden="true"></i></a>
+          <a class="cc-google-plus btn btn-link" href='#'><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
+          <a class="cc-instagram btn btn-link" href="#"><i class="fa fa-instagram fa-2x " aria-hidden="true"></i></a></div>
+      <div class="h4 title text-center">RIO AGUNG PRAYOGA</div>
+      <div class="text-center text-muted">
+      </div>
+    </footer>
+    <script src="<?= base_url()?>/assets/js/core/jquery.3.2.1.min.js?ver=1.1.0"></script>
+    <script src="<?= base_url()?>/assets/js/core/popper.min.js?ver=1.1.0"></script>
+    <script src="<?= base_url()?>/assets/js/core/bootstrap.min.js?ver=1.1.0"></script>
+    <script src="<?= base_url()?>/assets/js/now-ui-kit.js?ver=1.1.0"></script>
+    <script src="<?= base_url()?>/assets/js/aos.js?ver=1.1.0"></script>
+    <script src="<?= base_url()?>/assets/scripts/main.js?ver=1.1.0"></script>
+  </body>
+</html>
